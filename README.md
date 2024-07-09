6d438e7bccc812df2f5c6d5c4a005aeec4fc9d6d
Tristan Davis <Tristandavis888@gmail.com>
2024-07-08 19:44:08 -0700

    hello webpack

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - package.json 
     - remove main entry
     - set private true
+- npm install --save lodash
+- import lodash into script
+- `npx webpack` runs webpack script, looks for src/index.js
+- after run command, should view index.html from dist directory in browser to see 'Hello webpack'
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import _ from 'lodash'
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  // index.js assumes GLOBAL _ exists
+  // imported
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
